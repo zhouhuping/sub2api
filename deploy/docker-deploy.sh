d151777539ad17e63372f01e13e8063d949af8e1
jim <1014006164@qq.com>
2026-05-20 14:41:39 +0800

    修改docker安装文件的请求仓库

--- deploy/docker-deploy.sh
+++ deploy/docker-deploy.sh
@@ -21,7 +21,7 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # GitHub raw content base URL
-GITHUB_RAW_URL="https://raw.githubusercontent.com/Wei-Shaw/sub2api/main/deploy"
+GITHUB_RAW_URL="https://raw.githubusercontent.com/zhouhuping/sub2api/main/deploy"
 
 # Print colored message
 print_info() {
